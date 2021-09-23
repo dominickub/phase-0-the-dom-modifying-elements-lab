a3d5c4490c86af55a8dd1cb3f9813e69d83fbb35
dominick <dominickugas@gmail.com>
2021-09-23 14:50:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+document.querySelector(`main`).remove();
+
+
+const newHeader = document.createElement(`h1`)
+newHeader.setAttribute("id", "victory")
+// document.append(newHeader)
+// document.body.append(newHeader)
+// const h1 = document.createElement(`h1`)
+// h1.id = "victory"
+
+
+//     newHeader.id = "victory"
+// document.querySelector("h1#victory")
+// document.body.appendChild(newHeader)
+newHeader.innerHTML = "Dominick is the champion"
+
+
